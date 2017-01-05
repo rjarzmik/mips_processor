7736141a3562091f62effd9cf2b99fd00ecb76fc
Robert Jarzmik <robert.jarzmik@free.fr>
2017-01-05 22:35:52 +0100

    RegisterFile: switch to memory based registers
    Use a first twin registers memories for dual port access.
    Then use a specific accessor for double words storage, and reads, which
    are only allowed on first register index.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- DI/RegisterFile.vhd
+++ DI/RegisterFile.vhd
@@ -19,17 +19,53 @@
 -- 2016-11-12  1.0      rj      Created
 -------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 
--------------------------------------------------------------------------------
+entity regfile_memory is
+  generic (
+    DATA_WIDTH : positive;
+    NB_ENTRIES : positive
+    );
+  port (
+    clk       : in  std_logic;
+    stall_req : in  std_logic;
+    ridx      : in  natural range 0 to NB_ENTRIES - 1;
+    rdata     : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+    we        : in  std_logic;
+    widx      : in  natural range 0 to NB_ENTRIES - 1;
+    wdata     : in  std_logic_vector(DATA_WIDTH - 1 downto 0)
+    );
+end entity regfile_memory;
+
+architecture str of regfile_memory is
+  type r_array is array (0 to NB_ENTRIES - 1) of
+    std_logic_vector(DATA_WIDTH - 1 downto 0);
+  signal mem : r_array := (others => (others => '0'));
+
+begin  -- architecture str
+  process(clk, stall_req)
+  begin
+    if rising_edge(clk) then
+      if we = '1' then
+        mem(widx) <= wdata;
+      end if;
+      if stall_req = '0' then
+        rdata <= mem(ridx);
+      end if;
+    end if;
+  end process;
+end architecture str;
+
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity RegisterFile is
-
   generic (
     DATA_WIDTH        : positive := 32;
     NB_GP_REGISTERS   : positive := 32;  -- r0 to r31
-    NB_GPDW_REGISTERS : positive := 1    -- double width registers :
+    NB_GPDW_REGISTERS : positive := 2    -- double width registers :
                                          -- mf(mflo and mfhi)
     );
 
@@ -47,52 +83,91 @@ entity RegisterFile is
     q_a           : out std_logic_vector(DATA_WIDTH - 1 downto 0);
     q_b           : out std_logic_vector(DATA_WIDTH - 1 downto 0)
     );
-
 end entity RegisterFile;
 
--------------------------------------------------------------------------------
-
 architecture rtl of RegisterFile is
-  constant NB_REGS : natural := NB_GP_REGISTERS + NB_GPDW_REGISTERS;
+  signal r1_idx      : natural range 0 to NB_GP_REGISTERS - 1;
+  signal r2_idx      : natural range 0 to NB_GP_REGISTERS - 1;
+  signal w_idx       : natural range 0 to NB_GP_REGISTERS - 1;
+  signal rdw1_idx    : natural range 0 to NB_GPDW_REGISTERS - 1;
+  signal wdw_idx     : natural range 0 to NB_GPDW_REGISTERS - 1;
+  signal sw1_rdata   : std_logic_vector(DATA_WIDTH - 1 downto 0);
+  signal sw2_rdata   : std_logic_vector(DATA_WIDTH - 1 downto 0);
+  signal dw1_rdata   : std_logic_vector(DATA_WIDTH * 2 - 1 downto 0);
+  signal dw2sw_rdata : std_logic_vector(DATA_WIDTH - 1 downto 0);
 
-  type gp_array is array (0 to NB_GP_REGISTERS - 1) of
-    std_logic_vector(DATA_WIDTH - 1 downto 0);
-  type gpdw_array is array (0 to NB_GPDW_REGISTERS - 1) of
-    std_logic_vector(DATA_WIDTH * 2 - 1 downto 0);
-  signal gpregs   : gp_array   := (others => (others => '0'));
-  signal gpdwregs : gpdw_array := (others => (others => '0'));
+  signal sw_we : std_logic;
+  signal dw_we : std_logic;
+  signal wdata : std_logic_vector(DATA_WIDTH - 1 downto 0);
 
 begin  -- architecture rtl
+  r1_idx      <= a_idx mod NB_GP_REGISTERS;
+  r2_idx      <= b_idx mod NB_GP_REGISTERS;
+  w_idx       <= rwb_reg_idx mod NB_GP_REGISTERS;
+  rdw1_idx    <= (r1_idx / 2) mod NB_GPDW_REGISTERS;
+  dw2sw_rdata <= dw1_rdata(DATA_WIDTH - 1 downto 0) when (a_idx mod 2) = 0
+                 else dw1_rdata(DATA_WIDTH * 2 - 1 downto DATA_WIDTH);
 
-  process(rst, clk)
+  sw_we   <= '1' when rwb_reg_we = '1' and rwb_reg_idx < NB_GP_REGISTERS  else '0';
+  dw_we   <= '1' when rwb_reg_we = '1' and rwb_reg_idx >= NB_GP_REGISTERS else '0';
+  wdw_idx <= (rwb_reg_idx / 2) mod NB_GPDW_REGISTERS;
+  wdata   <= rwb_reg_wdata(DATA_WIDTH - 1 downto 0);
+
+  gprs_1 : entity work.regfile_memory
+    generic map (
+      DATA_WIDTH => DATA_WIDTH,
+      NB_ENTRIES => NB_GP_REGISTERS)
+    port map (
+      clk       => clk,
+      stall_req => stall_req,
+      ridx      => r1_idx,
+      rdata     => sw1_rdata,
+      we        => sw_we,
+      widx      => w_idx,
+      wdata     => wdata);
+  gprs_2 : entity work.regfile_memory
+    generic map (
+      DATA_WIDTH => DATA_WIDTH,
+      NB_ENTRIES => NB_GP_REGISTERS)
+    port map (
+      clk       => clk,
+      stall_req => stall_req,
+      ridx      => r2_idx,
+      rdata     => sw2_rdata,
+      we        => sw_we,
+      widx      => w_idx,
+      wdata     => wdata);
+  gpdwrs_1 : entity work.regfile_memory
+    generic map (
+      DATA_WIDTH => DATA_WIDTH * 2,
+      NB_ENTRIES => NB_GPDW_REGISTERS)
+    port map (
+      clk       => clk,
+      stall_req => stall_req,
+      ridx      => rdw1_idx,
+      rdata     => dw1_rdata,
+      we        => dw_we,
+      widx      => rdw1_idx,
+      wdata     => rwb_reg_wdata);
+
+  process(rst, a_idx, b_idx, sw1_rdata, sw2_rdata, dw2sw_rdata)
   begin
     if rst = '1' then
       q_a <= (others => 'X');
       q_b <= (others => 'X');
-    elsif rising_edge(clk) then
-      if rwb_reg_we = '1' then
-        if rwb_reg_idx < NB_GP_REGISTERS then
-          gpregs(rwb_reg_idx) <= rwb_reg_wdata(DATA_WIDTH - 1 downto 0);
-        else
-          gpdwregs(rwb_reg_idx) <= rwb_reg_wdata;
-        end if;
+    else
+      if a_idx < NB_GP_REGISTERS then
+        q_a <= sw1_rdata;
+      else
+        q_a <= dw2sw_rdata;
       end if;
-
-      if stall_req = '0' then
-        if a_idx < NB_GP_REGISTERS then
-          q_a <= gpregs(a_idx);
-        else
-          if (a_idx mod 2) = 0 then
-            q_a <= gpdwregs(a_idx / 2)(DATA_WIDTH - 1 downto 0);
-          else
-            q_a <= gpdwregs(a_idx / 2)(DATA_WIDTH * 2 - 1 downto DATA_WIDTH);
-          end if;
-        end if;
-        q_b <= gpregs(b_idx);
+      if b_idx < NB_GP_REGISTERS then
+        q_b <= sw2_rdata;
+      else
+        q_b <= dw2sw_rdata;
       end if;
     end if;
   end process;
 
 end architecture rtl;
 
--------------------------------------------------------------------------------
